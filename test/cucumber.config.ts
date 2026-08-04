060e8e33e983619667f55a8cc1c86962f5d41f48
Hang <2454043556@e.tlu.edu.vn>
2026-08-04 12:50:11 +0700

    fix: load cucumber config in github actions

--- test/cucumber.config.ts
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  paths: ["modules/**/*.feature"],
-  requireModule: ["tsx/cjs"],
-  require: ["shared/cucumber/**/*.ts", "modules/**/*.steps.ts"],
-  format: ["progress"],
-  publishQuiet: true,
-};
